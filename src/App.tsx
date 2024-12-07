07dbcd9e8df0976fc60bdb31695021e8ae0956dc
Akhmad Romadlon Zainur Rofiq <masterzera111@gmail.com>
2024-12-07 17:18:18 +0700

    remove heroicons

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState, Suspense } from "react";
 import { SearchBar } from "./components/SearchBar";
 import { ProjectList } from "./components/ProjectList";
 import { GithubProvider } from "./contexts/GithubContext";
-import { SunIcon } from "@heroicons/react/24/outline";
 import { Toaster } from "react-hot-toast";
 
 const App: React.FC = () => {
@@ -24,12 +23,9 @@ const App: React.FC = () => {
                         <Suspense
                             fallback={
                                 <div className="loading-container">
-                                    <SunIcon
-                                        width={24}
-                                        height={24}
-                                        color="#143E7F"
-                                        className="loading-indicator"
-                                    />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
+                                    </svg>
                                     <p>Loading readme...</p>
                                 </div>
                             }
